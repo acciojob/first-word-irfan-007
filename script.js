efb53c8b94f6f9f809e6a070ead275f4b7afc8aa
irfan <85334710+irfan-007@users.noreply.github.com>
2024-07-18 20:00:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+	let arr=s.split(" ");
+	if(arr.length==1)
+		return s;
+	return arr[0];
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s)); 
